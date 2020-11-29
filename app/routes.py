2841bcb6e07f558093db5d9a7474bbd30ba72410
heasleykr@gmail.com <Partytime1@!>
2020-11-28 19:55:13 -0800

    Initial commit for User Flask application.

---
+++ app/routes.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python3
+# -*- coding: utf8 -*-
+"""Routes file: Specifies CRUD Routes"""
+
+from flask import g
+from app import app
+import sqlite3
+
+DATABASE = "user.db"
+
---
+++ crud.py
@@ -0,0 +1,5 @@
+# !/usr/bin/env python3
+# -*- coding: utf8 -*-
+"""Main App Page: Flask app to store Users Information"""
+
+from app import app
